90192f1d2771d8997b6d89daaac52b7defc8d8f6
yuweilan <yuweilan@users.noreply.github.com>
2025-07-02 14:07:40 +0800

    feat: 初始化

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: yunzhen.yyz
+ * @Date: 2025-07-02 13:49:09
+ * @LastEditors: yunzhen.yyz
+ * @LastEditTime: 2025-07-02 13:49:15
+ * @Description: file content
+ * @FilePath: /mcp-test/src/app/pages/about.tsx
+ */
+function About() {
+  return <div>About</div>
+}
+
+export default About
---
+++ src/app/index/page.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: yunzhen.yyz
+ * @Date: 2025-07-02 13:48:09
+ * @LastEditors: yunzhen.yyz
+ * @LastEditTime: 2025-07-02 13:48:22
+ * @Description: file content
+ * @FilePath: /mcp-test/src/app/pages/index.tsx
+ */
+function HomePage() {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default HomePage
